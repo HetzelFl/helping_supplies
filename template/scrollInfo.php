bd79150ba37a48cd65230a828c3a2c22ac4b3daa
Florian <Florian@Florian-PC.fritz.box>
2016-01-07 21:55:15 +0100

    Paul image Link geändert

--- template/scrollInfo.php
+++ template/scrollInfo.php
@@ -1,3 +1,3 @@
-<a href="http://www.waterbackpack.org/" target="_blank">
+<a href="https://www.uni-kassel.de/fb14bau/institute/iwau/siedlungswasserwirtschaft/paul-wasserrucksack.html" target="_blank">
     <img src="/helping_supplies/images/paul_info.gif" alt="Infos zu Paul" style="position: fixed;bottom: 10%;left: 0px;"> 
 </a>
